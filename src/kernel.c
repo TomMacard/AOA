73dc8ec7b83bd1edb98b98cb28d72a7ed33e21f7
TomMacard <tom.macard@gmail.com>
2024-04-29 16:16:56 +0200

    aiaiaajzebi

--- src/kernel.c
+++ src/kernel.c
@@ -1,17 +1,6 @@
+
 #ifdef OPT1
 
-void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
-   unsigned i, j;
-   for (j=0; j<n; j++)
-      c[j]-=b[n-1-j]*n;
-   for (i=0; i<n; i++) {
-      for (j=i+1; j<n; j++)
-         c[i]+=a[i][j] ;
-   }
-}
-
-
-#elif defined OPT2
 
 /* opt , faire partir i de j au lieu de 0, et faire sauter le conditionnel */
 void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
@@ -21,6 +10,59 @@ void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
       for (i=j+1; i<n; i++) {
          c[j]+=a[j][i] ;
       }
+   }
+}
+
+
+
+#elif defined OPT2
+/* Reduction d'accés mémoire à c[] en utilisant une variable temporaire*/
+void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
+   unsigned i, j;
+   for (j=0; j<n; j++) {
+      c[j]-=b[n-1-j]*n;
+      double k = 0;
+      for (i=j+1; i<n; i++) {
+         k+=a[j][i] ;
+      }
+      c[j]+=k;
+   }
+}
+
+#elif defined OPT3
+/*Unrolling*/
+void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
+   unsigned i, j;
+
+   for(i=0; i<n; i++ ){
+      c[i]-=b[n-1-i]*n;
+   }
+
+   for(i=0; i<n-4; i=i+4){
+      
+      c[i]+=  a[i][i+1]   + a[i][i+2] +   a[i][i+3];
+      c[i+1]+=a[i+1][i+2] + a[i+1][i+3] + a[i+1][i+4];
+      c[i+2]+=a[i+2][i+3] + a[i+2][i+4] + a[i+2][i+5];
+      c[i+3]+=a[i+3][i+4] + a[i+3][i+5] + a[i+3][i+6];
+
+      for(j=i+4; j<n-4; j=j+4){
+         c[i]+=  a[i][j]     + a[i][j+1]   + a[i][j+2] +   a[i][j+3];
+         c[i+1]+=a[i+1][j+1] + a[i+1][j+2] + a[i+1][j+3] + a[i+1][j+4];
+         c[i+2]+=a[i+2][j+2] + a[i+2][j+3] + a[i+2][j+4] + a[i+2][j+5];
+         c[i+3]+=a[i+3][j+3] + a[i+3][j+4] + a[i+3][j+5] + a[i+3][j+6];
+      }
+
+      c[i]  += a[i][j]     + a[i][j+1]   + a[i][j+2]   + a[i][j+3];
+      c[i+1]+= a[i+1][j+1] + a[i+1][j+2] + a[i+1][j+3];
+      c[i+2]+= a[i+2][j+2] + a[i+2][j+3];
+      c[i+3]+= a[i+3][j+3];
+      
+   }
+
+   for(; i<n; i++){
+      for(j=i+1; j<n; j++){
+         c[i]+=a[i][j];
+      }   
    }
 }
 
@@ -37,5 +79,4 @@ void kernel (unsigned n, const double a[n][n], const double b[n], double c[n]) {
    }
 }
 #endif
-
 
